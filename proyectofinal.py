56a65a7c67c991e3a63bb9a3a3649da6f0361bda
alejoooo2007 <158303957+alejoooo2007@users.noreply.github.com>
2024-03-04 11:31:29 -0600

    proyecto final 1U

---
+++ proyectofinal.py
@@ -0,0 +1,8 @@
+listapalabras=[]
+for i in range (10):
+    palabra=input("ingrese palabra")
+    listapalabras.append(palabra)
+
+for i in range(len(listapalabras)):
+    print(listapalabras[i])
+    
