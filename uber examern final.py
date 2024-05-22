a61eac6e403dec4249e376d348aa498598d16498
alejoooo2007 <158303957+alejoooo2007@users.noreply.github.com>
2024-05-22 13:29:48 -0600

    Add files via upload

---
+++ uber examern final.py
@@ -0,0 +1,17 @@
+distaciarecorrida = 0
+tiempodeviaje= 0
+horadeldia= 0
+
+distaciarecorrida= float(input("Cual es su distancia recorrida en km: "))
+tiempodeviaje= float(input("Cual ha sido su tiempo de viaje en minutos: "))
+horadeldia = input("hora del dia la caul se llevo a cabo el viaje en horario 24 horas: ")
+
+distanciarecorrida = distaciarecorrida * 2.50
+tiempodeviaje = tiempodeviaje * 0.50
+
+if horadeldia == "7>= and 9<= and 17>= and 19<=": 
+    print(distaciarecorrida + tiempodeviaje + 5 * 0.20)
+
+else: horadeldia == "1>= and 6<= and 20>= and 24<= "
+print(5 + distaciarecorrida + tiempodeviaje)
+
